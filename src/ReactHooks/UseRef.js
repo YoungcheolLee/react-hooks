b8c1ca19dc11b9a819b808c853e2235280d119af
youngcheolLee <zodiac0905@gmail.com>
2023-02-21 18:20:26 +0900

    Update UseRef.js

--- src/ReactHooks/UseRef.js
+++ src/ReactHooks/UseRef.js
@@ -1,5 +1,34 @@
+import { useRef, useState } from "react";
+
 const UseRef = () => {
-  return <div>This is UseRef Page</div>;
+  const inputName = useRef();
+  const introduceInput = useRef();
+  const [name, setName] = useState({ name: "", introduce: "" });
+
+  const handleInputChange = (e) => {
+    if(name.inputName.length < 1) {
+      alert("이름을 입력해주세요!");
+      inputName.current.focus();
+      return;
+    }
+
+    if (name.introduce.length < 1) {
+      alert("자기소개를 입력해주세요!");
+      introduceInput.current.focus();
+      return;
+    }
+
+    alert("저장성공!");
+  }
+
+  return (
+    <div>
+      This is UseRef Page
+      <div>
+        <input ref="name" value={name} placeholder={"이름을 입력해주세요!"} onChange={handleInputChange}} />
+      </div>
+    </div>
+  );
 };
 
 export default UseRef;
