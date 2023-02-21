f7d148752480aa923208b2b473a362488517b5b4
youngcheolLee <zodiac0905@gmail.com>
2023-02-22 00:59:02 +0900

    UseRef.js 컴포넌트 DOM 요소 접근 예제 추가(로그인)

--- src/ReactHooks/UseRef.js
+++ src/ReactHooks/UseRef.js
@@ -4,14 +4,21 @@ useRef와 useState의 차이점,
 2. useRef : ref의 값이 변화하더라도 rendering이 되지 않는다.
 
 컴포넌트 안의 변수와 useRef의 차이점
-1. 
+ - 렌더링 시점에 변수 > 데이터 초기화 / useRef > 데이터 유지
 */
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const UseRef = () => {
   const [count, setCount] = useState(0);
   const countRef = useRef(0);
   let myVariable = 0;
+  const inputRef = useRef();
+
+  // rendering 시 inputRef DOM요소에 자동으로 포커스되는 기능 추가
+  useEffect(() => {
+    console.log("inputRef :", inputRef);
+    inputRef.current.focus();
+  }, []);
 
   console.log("countRef =", countRef);
 
@@ -30,14 +37,43 @@ const UseRef = () => {
     console.log("myVariable = ", myVariable);
   };
 
+  const onCheckEnter = (e) => {
+    if (e.key === "Enter") {
+      return handleLogin();
+    }
+  };
+
+  const handleLogin = () => {
+    if (inputRef.current.value.length < 1) {
+      alert("ID를 입력해주세요!");
+    } else {
+      alert(`${inputRef.current.value}님 환영합니다!`);
+    }
+  };
+
   return (
     <div className="useRef-wrraper">
+      <h3>React Hook - useRef</h3>
+      <hr />
       <p>COUNT : {count}</p>
       <p>countRef : {countRef.current}</p>
       <p>myVariable : {myVariable}</p>
-      <button onClick={increaseCount}>COUNT 증가</button>
-      <button onClick={increaseCountRef}>countRef 증가</button>
-      <button onClick={increaseVariable}>myVaribale 증가</button>
+      <div className="useRef-button">
+        <button onClick={increaseCount}>COUNT 증가</button>
+        <button onClick={increaseCountRef}>countRef 증가</button>
+        <button onClick={increaseVariable}>myVaribale 증가</button>
+      </div>
+      <hr />
+      <div className="useRef-login">
+        <h3>useRef를 사용해 DOM요소 접근</h3>
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="ID를 입력해주세요"
+          onKeyUp={onCheckEnter}
+        />
+        <button onClick={handleLogin}>로그인</button>
+      </div>
     </div>
   );
 };
